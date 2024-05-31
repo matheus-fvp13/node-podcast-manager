22e08d75a9bec04d16af0986b0b1b2632e0c9be4
Matheus Vieira <matheus.fvp@outlook.com>
2024-05-31 11:17:00 -0300

    podcasts-controller

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -3,12 +3,19 @@ import { IncomingMessage, ServerResponse } from "http";
 export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(
-        JSON.stringify({
-            podcastName: "flow",
-            episode: "CBUM - Flow #319",
-            videoId: "pQSuQmUfS30",
-            cover: "https://i.ytimg.com/vi/pQSuQmUfS30/hqdefault.jpg",
-            link: "https://www.youtube.com/watch?v=pQSuQmUfS30",
-            categories: ["saúde", "esporte", "bodybuilder"]
-        }));
+        JSON.stringify([
+            {
+                podcastName: "flow",
+                episode: "CBUM - Flow #319",
+                videoId: "pQSuQmUfS30",
+                categories: ["saúde", "esporte", "bodybuilder"]
+            },
+            {
+                podcastName: "flow",
+                episode: "PETER JORDAN + BRENO JORDAN - Flow #350",
+                videoId: "RejZhH-Dfjs",
+                categories: ["tecnologia", "comics", "animes"]
+            }
+        ])
+    );
 };
