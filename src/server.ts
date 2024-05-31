369c70fb121d4ce168cc885f84453ccc1c2b2324
Matheus Vieira <matheus.fvp@outlook.com>
2024-05-31 10:47:02 -0300

    node http server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import * as http from "http";
+
+const server = http.createServer(
+    (req: http.IncomingMessage, res: http.ServerResponse) => {}
+);
+
+const port = process.env.SERVER_PORT;
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
