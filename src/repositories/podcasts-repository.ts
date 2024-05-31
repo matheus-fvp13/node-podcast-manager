fef932f840edf68e697f957010f39253ec6e97fc
Matheus Vieira <matheus.fvp@outlook.com>
2024-05-31 12:04:11 -0300

    dinamic module

--- src/repositories/podcasts-repository.ts
+++ src/repositories/podcasts-repository.ts
@@ -2,7 +2,7 @@ import fs from "fs"
 import path from "path"
 import { Podcast } from "../models/podcast-model";
 
-const pathData = path.join(__dirname, "../repositories/podcast.json");
+const pathData = path.join(__dirname, "../repositories/podcasts.json");
 
 export const repositoryPodcast = async (): Promise<Podcast[]> => {
     const rawData = fs.readFileSync(pathData, "utf-8");
