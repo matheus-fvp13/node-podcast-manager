25406569b32c8a92d74ffb114febceadd628a54e
Matheus Vieira <matheus.fvp@outlook.com>
2024-06-03 10:03:33 -0300

    remove magic number and floating text

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import * as http from "http";
 import { getFilterEpisodes, getListEpisodes } from "./controllers/podcasts-controller";
 import { Routes } from "./routes/routes";
+import { HttpMethods } from "./utils/http-methods";
 
 const server = http.createServer(
-    async (req: http.IncomingMessage, res: http.ServerResponse) => {
-        const [baseUrl, queryString] = req.url?.split("?") ?? ["", ""];
+    async (request: http.IncomingMessage, response: http.ServerResponse) => {
+        const [baseUrl, queryString] = request.url?.split("?") ?? ["", ""];
 
-        if(req.method === "GET" && baseUrl === Routes.LIST) {
-            await getListEpisodes(req, res);
+        if(request.method === HttpMethods.GET && baseUrl === Routes.LIST) {
+            await getListEpisodes(request, response);
         }
 
-        if(req.method === "GET" && baseUrl === Routes.EPISODE) {
-            await getFilterEpisodes(req, res);
+        if(request.method === HttpMethods.GET && baseUrl === Routes.EPISODE) {
+            await getFilterEpisodes(request, response);
         }
     }
 );
